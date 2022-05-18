c8036333c4274b05800a51ac918c957cda118017
IT21295874 <99392634+IT21295874@users.noreply.github.com>
2022-05-19 03:28:43 +0530

    Create Admin.cpp

---
+++ Admin.cpp
@@ -0,0 +1,33 @@
+#inculde"Admin.h"
+#include<iostream>
+using namespace std;
+#include<cstring>
+
+
+
+
+Admin::Admin()
+{
+strcpy(addName,"");
+adminId=0;
+}
+Admin::Admin(int adId,char adName[]);
+{
+adminId=aID;
+strcpy(addName,"adName");
+}
+void Admin::setaddName(char adname[])
+{
+}
+void Admin::displayDetails()
+{
+}
+
+
+
+Admin::~Admin()
+{
+
+
+
+};
