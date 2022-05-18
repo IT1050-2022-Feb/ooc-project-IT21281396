3175da51b21107a18054dfcb159792f64b8e12e6
it21344756 <99312095+it21344756@users.noreply.github.com>
2022-05-19 00:58:25 +0530

    Create Food.cpp

---
+++ Food.cpp
@@ -0,0 +1,51 @@
+#include "Admin.h"
+#include<iostream>
+using name std;
+#include<cstring>
+
+
+Food::Food()
+{
+   strcpy(F_Name,"");
+   F_No=0;
+   F_Price=0;
+   Qty=0;
+   
+}
+Food::Food(Food( char fName[], int fNo, int fPrice, int Qty);)
+{
+ F_name=fName
+ F_No=fNo;
+ F_Price=fPrice;
+ qty=Qty;
+
+
+}
+void Food::Qty(int iQty)
+{
+  
+}
+void Food::setF_Name(char fName[])
+{
+  
+}
+void Food::setF_No( int fNo)
+{
+  
+}
+void Food::setfPrice(int fPrice)
+{
+
+}
+void Food::displayItemDetails()
+{
+
+}
+void Food::updateItemDetails()
+{
+
+}
+Food::~Food()
+{
+//Destructor
+}
